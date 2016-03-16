c0c55f969602fcff08ec3408565a05ca94e5edd8
lm1983111 <315548007@qq.com>
2016-03-16 21:04:41 +0800

    台式机本地资源第一次提交

---
+++ js/tenvee.js
@@ -0,0 +1,17 @@
+//window.onload = function() {
+//	//获取节点a
+//	var nla = $(".nav > li > a");
+//	//移到菜单上面，兄弟节点显示，否则隐藏
+//	nla.hover(function() {
+//			$(this).next(".nav-full-container").show();
+//	},
+//		function() {
+//			$(this).next(".nav-full-container").hide();
+//		})
+//	//移到子菜单上面后，自己显示，否则隐藏
+//	$(".nav-full-container").hover(function() {
+//		$(this).show();
+//	}, function() {
+//		$(this).hide();
+//	})
+//}

37b1150fe0633ff3949a693c0cf4f9d1f02d3ab8
lm1983111 <315548007@qq.com>
2016-03-17 08:42:11 +0800

    合并公司电脑ui-collection页面

---
+++ js/a.js
@@ -0,0 +1,22 @@
+//通用申明函数
+//function myFunction() {
+//	alert("dddd")
+//}
+//myFunction();
+
+//使用块作用域申明函数
+//(function() {
+//	function myFunction() {
+//		alert("dddd")
+//	}
+//	myFunction();
+//})()
+
+//使用require调用加载文件
+define(function() {
+	function myFunction() {
+		alert("dddd")
+	}
+	myFunction();
+})
+
